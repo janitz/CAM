03f961079d6e8b4f075a571190315915c4b5dde9
janitz <janis-d@web.de>
2017-02-04 15:50:15 +0100

    initial commit

---
+++ html/main.js
@@ -0,0 +1,9 @@
+var canv = document.getElementById("canvas");
+var txtArea = document.getElementById("textarea");
+
+canv.width = window.innerWidth / 2.1;
+canv.height = window.innerHeight - 20;
+txtArea.style.width = (window.innerWidth / 2.1) + "px";
+txtArea.style.height = (window.innerHeight - 20) + "px";
+
+
