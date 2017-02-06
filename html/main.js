e305ab14def41adc59ada1748ce3e7c912913dc2
janitz <janis-d@web.de>
2017-02-06 06:31:18 +0100

    some of the first tests

--- html/main.js
+++ html/main.js
@@ -1,9 +1,35 @@
+
 var canv = document.getElementById("canvas");
 var txtArea = document.getElementById("textarea");
+var debug = document.getElementById("debugger");
 
-canv.width = window.innerWidth / 2.1;
-canv.height = window.innerHeight - 20;
-txtArea.style.width = (window.innerWidth / 2.1) + "px";
-txtArea.style.height = (window.innerHeight - 20) + "px";
 
+window.addEventListener('load', resize);
+window.addEventListener('resize', resize);
+txtArea.addEventListener('change', parse);
+
+
+function resize(e) {
+    canv.width = window.innerWidth / 2.1;
+    canv.height = window.innerHeight - 20;
+    txtArea.style.width = (window.innerWidth / 2.1) + "px";
+    txtArea.style.height = (window.innerHeight - 20) + "px";
+}
+
+function parse(e) {
+    var lines = txtArea.value.split("\n");
+    var txt = ""
+    for(var i = 0; i < lines.length; i++){
+        txt += lines[i] + "<br>";
+    }
+    debug.innerHTML = txt;
+}
+
+function parseLine(txt) {
+    
+}
+
+function draw(){
+
+}
 
