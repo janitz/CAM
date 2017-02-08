4f3720f8d67b6b2a1f66b304ccc5abadffb19ddb
janitz <janis-d@web.de>
2017-02-08 08:01:59 +0100

    first tests

--- html/main.js
+++ html/main.js
@@ -1,35 +1,44 @@
-
-var canv = document.getElementById("canvas");
+var menuField = document.getElementById('menu');
+var cmdField = document.getElementById('cmd');
 var txtArea = document.getElementById("textarea");
-var debug = document.getElementById("debugger");
+var canvas = document.getElementById('canv');
+var context = canvas.getContext('2d');
+var mPosField = document.getElementById('mousePos');
 
+function writeMessage(canvas, message) {
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = '18pt Calibri';
+    context.fillStyle = 'white';
+    context.fillText(message, 10, 25);
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: rect.height - (evt.clientY - rect.top) - 1
+    };
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    mPosField.innerHTML = mousePos.x + ', ' + mousePos.y + '&nbsp';
+
+    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    writeMessage(canvas, message);
+}, false);
 
 window.addEventListener('load', resize);
 window.addEventListener('resize', resize);
-txtArea.addEventListener('change', parse);
 
-
-function resize(e) {
-    canv.width = window.innerWidth / 2.1;
-    canv.height = window.innerHeight - 20;
-    txtArea.style.width = (window.innerWidth / 2.1) + "px";
-    txtArea.style.height = (window.innerHeight - 20) + "px";
-}
-
-function parse(e) {
-    var lines = txtArea.value.split("\n");
-    var txt = ""
-    for(var i = 0; i < lines.length; i++){
-        txt += lines[i] + "<br>";
-    }
-    debug.innerHTML = txt;
-}
-
-function parseLine(txt) {
+function resize(evt){
     
+    var w = window.innerWidth;
+    var h = window.innerHeight - menuField.clientHeight - cmdField.clientHeight - mPosField.clientHeight - 6;
+    canvas.width = Math.floor(w / 3 * 2);
+    canvas.height = h;
+    txtArea.style.width = Math.floor(w / 3) + "px";
+    txtArea.style.height = h  + "px";
+    mPosField.innerHTML = "resized  to: "  + w + ', ' + h + '&nbsp';
 }
-
-function draw(){
-
-}
-
